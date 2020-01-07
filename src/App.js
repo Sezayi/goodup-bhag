d3dde35223d20da213f02b10451ba44a0c37ab9c
Sezayi Arslan <arslan.sezayi@gmail.com>
2020-01-07 15:41:56 +0100

    add new bhag number

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import star from "./images/star.svg";
 import sdgs from "./images/sdgs.png";
 
 function App() {
-  const value = 2341223;
+  const value = 77288;
 
   const spring = useSpring({
     config: { duration: 2000 },
